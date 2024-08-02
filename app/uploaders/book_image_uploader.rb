e79821d2f49c3939f0e1b355fce6aed12f29f20d
ce-ta <asahitsuchida7865@gmail.com>
2024-08-02 17:36:15 +0900

    commit

--- app/uploaders/book_image_uploader.rb
+++ app/uploaders/book_image_uploader.rb
@@ -1,6 +1,11 @@
 class BookImageUploader < CarrierWave::Uploader::Base
   storage :file
 
+  # 保存先のディレクトリを指定する
+  def store_dir
+    'public/uploads/book'
+  end
+  
   # 画像の拡張子の許可
   def extension_whitelist
     %w(jpg jpeg png)
